96174ec71e0b133efecc5b7e291fda49d3665e4b
Mark Diekhans <markd@ucsc.edu>
2023-04-27 09:42:12 -0700

    add program to install dependencies

---
+++ install_R_packages.R
@@ -0,0 +1,42 @@
+library(devtools)
+
+# functions to install a package if it's missing
+install_if_missing <- function(package) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    install.packages(package)
+  }
+}
+install_github_if_missing <- function(package, repo) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    devtools::install_github(repo)
+  }
+}
+
+packages <- c(
+  "LSD",
+  "MASS",
+  "RColorBrewer",
+  "cowplot",
+  "devtools",
+  "dplyr",
+  "ggplot2",
+  "ggprism",
+  "ggpubr",
+  "ggridges",
+  "ggthemes",
+  "grid",
+  "gridExtra",
+  "pheatmap",
+  "plyr",
+  "scales",
+  "viridis"
+)
+
+lapply(packages, install_if_missing)
+
+# Load the packages
+lapply(packages, library, character.only = TRUE)
+
+# Local Variables:
+# ess-indent-offset: 2
+# End:
