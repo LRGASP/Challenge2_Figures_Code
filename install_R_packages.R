9acaa72ca90c4dbe1418631fea0bf2b3f678406a
Mark Diekhans <markd@ucsc.edu>
2023-06-04 10:12:26 -0700

    added missing package install

--- install_R_packages.R
+++ install_R_packages.R
@@ -1,11 +1,12 @@
-library(devtools)
-
 # functions to install a package if it's missing
 install_if_missing <- function(package) {
   if (!requireNamespace(package, quietly = TRUE)) {
     install.packages(package)
   }
 }
+
+install_if_missing("devtools")
+
 install_github_if_missing <- function(package, repo) {
   if (!requireNamespace(package, quietly = TRUE)) {
     devtools::install_github(repo)
@@ -20,6 +21,7 @@ packages <- c(
   "devtools",
   "dplyr",
   "ggplot2",
+  "ggplotify",
   "ggpointdensity",
   "ggprism",
   "ggpubr",
